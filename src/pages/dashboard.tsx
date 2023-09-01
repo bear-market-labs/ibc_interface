f7a7b51f8e348e78a519d1f5af39498456a1dc03
ibjc <2389015+ibjc@users.noreply.github.com>
2023-09-01 15:36:46 +0000

    radio group for left nav panel

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,12 +1,51 @@
-import { Code, Divider, Grid, GridItem, Link, Spacer, Stack, Tab, Text, VStack } from "@chakra-ui/react";
+import { Code, Divider, Grid, GridItem, Link, RadioGroup, Spacer, Stack, Tab, Text, useRadioGroup, VStack } from "@chakra-ui/react";
+import { time } from "console";
+import { useState } from "react";
 import { ColorModeSwitcher } from "../ColorModeSwitcher";
 import ConnectWallet from "../components/ConnectWallet";
+import { RadioCard } from "../components/radio_card";
 import CreatePool from "../components/staging/create_pool";
 import MaxTokenApprove from "../components/staging/max_token_approval";
 import WrapEth from "../components/staging/wrap_eth";
 
 export function Dashboard(){
+  const navOptions = [
+    {
+      value: 'mintBurn',
+      displayText: 'Mint / Burn'
+    },
+    {
+      value: 'lp',
+      displayText: 'Add / Remove Liquidity'
+    },
+    {
+      value: 'claim',
+      displayText: 'Claim'
+    }
+  ]
 
+  const [selectedNavItem, setSelectedNavItem] = useState<string>(navOptions[0].value);
+
+
+  // data to fetch 
+  // contract's current params
+  // contract's current reserves
+  // contract's current supply
+
+  // data to fetch if wallet connected
+  // user's token balance
+  // user's eth balance
+  // user's lp balance
+  // user's claimable fees 
+
+  // data to generate
+  // curve graph plot points
+
+  const { getRootProps, getRadioProps } = useRadioGroup({
+    name: 'vaults',
+    onChange: (val) => setSelectedNavItem(val),
+  })
+  const group = getRootProps()
 
   return (
     <Grid
@@ -15,19 +54,27 @@ export function Dashboard(){
         "sidenav vertline1 horizline horizline horizline"
         "sidenav vertline1 main vertline2 sideinput"`}
             gridTemplateRows={'70px 1px 1fr'}
-            gridTemplateColumns={'150px 1px 2fr 1px 1fr'}
+            gridTemplateColumns={'200px 1px 2fr 1px 1fr'}
             gap='0'
     >
       <GridItem area={'sidenav'}>
-        <Stack spacing={0}>
-          <Text ml={3} align={"left"}>INVERSE</Text>
-          <Text ml={3} align={"left"}>BONDING</Text>
-          <Text ml={3} align={"left"}>CURVE</Text>
-          {/*radio group
-            mint/burn
-            lp
-            claim
-          */}
+        <Stack spacing={10}>
+          <Stack spacing={0}>
+            <Text ml={3} align={"left"}>INVERSE</Text>
+            <Text ml={3} align={"left"}>BONDING</Text>
+            <Text ml={3} align={"left"}>CURVE</Text>
+          </Stack>
+
+          <Stack {...group} spacing={3}>
+            {navOptions.map((item) => {
+              const radio = getRadioProps({ value: item.value })
+              return(
+                <RadioCard key={item.value} {...radio}>
+                  <Text align="left">{item.displayText}</Text>
+                </RadioCard>
+              )
+            })}
+          </Stack>
         </Stack>
       </GridItem>
 

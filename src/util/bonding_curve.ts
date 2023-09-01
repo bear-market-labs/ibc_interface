b51b9d9d559b0313af4039a93ca67cc2fc434b8b
ibjc <2389015+ibjc@users.noreply.github.com>
2023-09-01 06:11:26 +0000

    implement mint/burn integral calcs

--- src/util/bonding_curve.ts
+++ src/util/bonding_curve.ts
@@ -1,22 +1,49 @@
-export function generateCurve(m: number, s: number, q: number, maxX: number, numPoints: number){
+export function generateCurve(p0: number, x: number, s0: number, r0: number, maxX: number, numPoints: number){
   let datapoints = [];
+  const m = p0 * Math.pow(1/s0, p0*s0/r0)
+  const q = p0*s0/r0 - 1
+
   for (let i = 1; i < maxX; i+=Math.floor(maxX/numPoints)){
-    datapoints.push({x: i, y: m*Math.pow(s, q)})
+    datapoints.push({x: i, y: m*Math.pow(i, q)})
   }
 
   return datapoints
 }
 
-export function amountToMint(reserveAmountIn: number, m: number, s: number, q: number){
+export function amountToMint(reserveAmountIn: number, p0: number, x: number, s0: number, r0: number){
   // formulate intergral expression
+  const m = p0 * Math.pow(1/s0, p0*s0/r0)
+  const q = p0*s0/r0 - 1
+
+  const q_1 = p0*s0/r0
+  const m_1 = m * q_1
+
+  const integralAtStartSupply = m_1 * Math.pow(x, q_1)
+  const integralAtEndSupply = reserveAmountIn + integralAtStartSupply
+
+  // F(end) - F(start) = R
+  // F(end) = R + F(start)
+  // end**q_1 = (R + F(start)) / m_1
+  // ln(end) = (1 / q_1) * ln((R + F(start)) / m_1)
+  // end = exp( (1 / q_1) * ln(F(end) / m_1) )
+
+  return  Math.exp(1/q_1 * Math.log(integralAtEndSupply / m_1))
 }
 
-export function areaUnderBondingCurve(startSupply: number, endSupply: number, m: number, q: number){
+export function areaUnderBondingCurve(startSupply: number, endSupply: number, p0: number, s0: number, r0: number){
   // perform definite integral, and return area under the curve
+  const m = p0 * Math.pow(1/s0, p0*s0/r0)
+  const q = p0*s0/r0 - 1
+
+  const q_1 = p0*s0/r0
+  const m_1 = m * q_1
+
+  const integralAtStartSupply = m_1 * Math.pow(startSupply, q_1)
+  const integralAtEndSupply = m_1 * Math.pow(endSupply, q_1)
+
+  return integralAtEndSupply - integralAtStartSupply
 }
 
-export function bondingCurveTwist(m: number, s: number, q: number, reserveAmountIn: number){
-  // curve update, constrained by:
-  // - same spot price
-  // - same token supply
+export function claimableLpYield(globalIndex: number, userIndex: number, userAccruedYield: number, userLpAmount: number){
+  return userLpAmount * (globalIndex - userIndex) + userAccruedYield
 }

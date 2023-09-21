748287965f4df0010b692bf7651430258cb075f5
kimchicoffee <pasdesujin@gmail.com>
2023-09-21 12:30:48 -0400

    feat: add external links on side nav

---
+++ src/components/dashboard/external_links.tsx
@@ -0,0 +1,22 @@
+import { Flex, Icon, Link } from "@chakra-ui/react";
+import { FaGithub } from "react-icons/fa";
+import { RiTwitterXFill } from "react-icons/ri";
+import { SiGitbook } from "react-icons/si";
+
+const links = [
+	{ address: "https://github.com/bear-market-labs", icon: FaGithub },
+	{ address: "https://x.com/bearmarketlabs", icon: RiTwitterXFill },
+	{ address: "https://github.com/bear-market-labs", icon: SiGitbook },
+];
+
+const ExternalLinks = () => (
+	<Flex gap='7' ml='7' mb='7' flexGrow='0' flexShrink={0}>
+		{links.map((link) => (
+			<Link href={link.address} isExternal>
+				<Icon as={link.icon} fontFamily='l' />
+			</Link>
+		))}
+	</Flex>
+);
+
+export default ExternalLinks;

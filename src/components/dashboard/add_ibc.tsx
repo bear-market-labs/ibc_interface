9f2d04afc6de9175758031cdbd5dc26566ed4dc0
ibjc <2389015+ibjc@users.noreply.github.com>
2023-09-27 20:14:50 +0000

    show disabled styling when wallet isn't connected

--- src/components/dashboard/add_ibc.tsx
+++ src/components/dashboard/add_ibc.tsx
@@ -1,38 +1,29 @@
 import { useCallback, useEffect, useState } from 'react'
 import { useConnectWallet } from '@web3-onboard/react'
 import {  ethers, BigNumber } from 'ethers'
-import { Button, Input, Stack } from '@chakra-ui/react'
+import { Button, Input, Link, Stack } from '@chakra-ui/react'
 import { contracts } from '../../config/contracts'
+import { HiExternalLink } from 'react-icons/hi'
+import { BiLinkExternal } from 'react-icons/bi'
+import { isAbleToSendTransaction } from '../../config/validation'
+import { colors } from '../../config/style'
 
-type mintProps = {
-  dashboardDataSet: any;
-  parentSetters: any;
+type addProps = {
+  tokenDecimals: any;
+  tokenAddress: any;
+  tokenSymbol: any;
 }
 
-export default function AddIbc(props: mintProps) {
+export default function AddIbc(props: addProps) {
   const [{ wallet, }] = useConnectWallet()
-  const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>()
   const [ibcContractAddress, ] = useState<string>(contracts.tenderly.ibcContract)
-  const {dashboardDataSet, } = props
-
-  const tokenDecimals = BigNumber.from("inverseTokenDecimals" in dashboardDataSet ? dashboardDataSet.inverseTokenDecimals : '0'); 
-  const tokenAddress = "inverseTokenAddress" in dashboardDataSet ? dashboardDataSet.inverseTokenAddress : '';
-  const tokenSymbol = "inverseTokenSymbol" in dashboardDataSet ? dashboardDataSet.inverseTokenSymbol : '';
-  
   const tokenImage = 'http://placekitten.com/200/300';
-
-
+  const {tokenDecimals, tokenAddress, tokenSymbol} = props;
 
   const sendTransaction = useCallback(async () => {
 
-    if (!wallet){
+    if (!wallet || !wallet?.provider){
       return
-    }
-
-    if (wallet?.provider) {
-      setProvider(new ethers.providers.Web3Provider(wallet.provider, 'any'))
-      // if using ethers v6 this is:
-      // ethersProvider = new ethers.BrowserProvider(wallet.provider, 'any')
     }
   
     try {
@@ -58,13 +49,24 @@ export default function AddIbc(props: mintProps) {
     } catch (error) {
       console.log(error);
     }
-  }, [wallet, provider, ibcContractAddress]);
+  }, [wallet, ibcContractAddress, tokenAddress, tokenDecimals, tokenSymbol, tokenImage]);
 
   return (
     <>
-      <Stack direction="row">
-        <Button onClick={sendTransaction}>Add IBC</Button>
-      </Stack>
+      {
+        isAbleToSendTransaction(wallet, wallet?.provider, 69)&&(
+          <Link onClick={sendTransaction} isExternal>
+          <BiLinkExternal></BiLinkExternal>
+          </Link>
+        )
+      }
+
+      {
+        !isAbleToSendTransaction(wallet, wallet?.provider, 69)&&(
+          <BiLinkExternal color={colors.GRAYED_OUT_GRAY}></BiLinkExternal>
+        )
+      }
+
     </>
   )
 }

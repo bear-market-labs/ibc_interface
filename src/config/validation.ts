c25e1220cede54a7182e7eed5fccf95131e725ec
kimchicoffee <pasdesujin@gmail.com>
2023-09-27 15:12:55 -0400

    fix: disable buttons when it fails the validation (#101)

---
+++ src/config/validation.ts
@@ -0,0 +1,7 @@
+export const isAbleToSendTransaction = (wallet: any, provider: any, amount?: any) => {
+    if (!wallet || !provider || !amount || amount <= 0) {
+        return false
+    }
+
+    return true
+}

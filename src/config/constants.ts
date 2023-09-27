8f6b6fd9a4330a83fd94b6088795c2590b9b1022
ibjc <2389015+ibjc@users.noreply.github.com>
2023-09-27 20:18:02 +0000

    ibc image url constant

--- src/config/constants.ts
+++ src/config/constants.ts
@@ -14,3 +14,5 @@ export const actionTypes = [
 
 export const format = (val: any) => val + `%`
 export const parse = (val: any) => val.replace(/^\%/, '')
+
+export const ibcImageUrl = 'http://placekitten.com/200/300'

003f5e8c51e74ee9e76ca0d8c6bbeb458af6e7c6
ibjc <2389015+ibjc@users.noreply.github.com>
2023-09-27 21:08:40 +0000

    add rewards for lp page

--- src/components/dashboard/lping_reserve.tsx
+++ src/components/dashboard/lping_reserve.tsx
@@ -1,7 +1,8 @@
 import {  ethers } from 'ethers'
-import { Box, Icon, Stack, Text } from '@chakra-ui/react'
+import { Box, Center, Divider, Icon, Stack, Text } from '@chakra-ui/react'
 import { BigNumber } from 'ethers'
 import { HiOutlineArrowRight} from "react-icons/hi"
+import { blocksPerDay } from '../../config/constants'
 
 type mintProps = {
   dashboardDataSet: any;
@@ -15,23 +16,42 @@ export default function LpingReserve(props: mintProps) {
 
   const reserveAmount = "reserveAmount" in bondingCurveParams ? BigNumber.from(bondingCurveParams.reserveAmount).sub(virtualReserveAmount) : BigNumber.from('0'); 
   const newReserve = parentInputDynamicData?.newReserve ? BigNumber.from(parentInputDynamicData.newReserve).sub(virtualReserveAmount) : BigNumber.from('0'); 
+  const lpRewardEma = "lpRewardEma" in dashboardDataSet ? dashboardDataSet.lpRewardEma : {
+    reserveAsset: 0,
+    ibcAsset: 0
+  }; 
+  const inverseTokenDecimals = "inverseTokenDecimals" in dashboardDataSet ? dashboardDataSet.inverseTokenDecimals : '0'; 
 
   return (
     <>
-      <Stack>
-      <Text ml={7} mt={7} align="left">RESERVE</Text>
       <Stack direction="row">
-        <Text ml={7} align="left">{`${Number(ethers.utils.formatEther(reserveAmount)).toFixed(3)} ETH`}</Text>
-        {
-          newReserve.gt(0) && !newReserve.eq(reserveAmount) && 
-          <>
-            <Box ml='7' mr='7'>
-              <Icon as={HiOutlineArrowRight} h='100%'/>
-            </Box>
-            <Text>{`${Number(ethers.utils.formatEther(newReserve)).toFixed(3)} ETH`}</Text>
-          </>
-        }
-      </Stack>
+        <Stack>
+          <Text ml={7} mt={7} align="left">RESERVE</Text>
+          <Stack direction="row">
+            <Text ml={7} align="left">{`${Number(ethers.utils.formatEther(reserveAmount)).toFixed(3)} ETH`}</Text>
+            {
+              newReserve.gt(0) && !newReserve.eq(reserveAmount) && 
+              <>
+                <Box ml='7' mr='7'>
+                  <Icon as={HiOutlineArrowRight} h='100%'/>
+                </Box>
+                <Text>{`${Number(ethers.utils.formatEther(newReserve)).toFixed(3)} ETH`}</Text>
+              </>
+            }
+          </Stack>
+        </Stack>
+        <Center mt={7} ml={450} height='69px'>
+          <Divider orientation='vertical' colorScheme={'gray'} />
+        </Center>
+        <Stack ml={50} align={'right'}>
+          <Text  mt={7} align="left">APPROX LP REWARDS</Text>
+            {
+              <>
+
+                <Text>{`${Number(Number(ethers.utils.formatEther(lpRewardEma.reserveAsset))* blocksPerDay).toFixed(3)} ETH + ${Number(Number(ethers.utils.formatUnits(lpRewardEma.ibcAsset, inverseTokenDecimals))* blocksPerDay).toFixed(3)} IBC`}</Text>
+                </>
+            }
+        </Stack>
       </Stack>
     </>
   )

656cba1dea35c1a5a402a24ce0dd16f18b48875a
ibjc <2389015+ibjc@users.noreply.github.com>
2023-10-18 05:07:38 +0000

    stake/unstake updated for router

--- src/components/dashboard/stake_ibc.tsx
+++ src/components/dashboard/stake_ibc.tsx
@@ -6,7 +6,7 @@ import { arrayify, concat, defaultAbiCoder, hexlify, formatUnits, parseEther, pa
 import { BigNumber } from 'ethers'
 import { contracts } from '../../config/contracts'
 import { DefaultSpinner } from '../spinner'
-import { explorerUrl } from '../../config/constants'
+import { commandTypes, explorerUrl } from '../../config/constants'
 import { Toast } from '../toast'
 import { BiLinkExternal } from 'react-icons/bi'
 import { error_message } from '../../config/error'
@@ -20,7 +20,8 @@ type mintProps = {
 export default function StakeIbc(props: mintProps) {
   const [{ wallet, connecting }] = useConnectWallet()
   const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>()
-  const [ibcContractAddress, ] = useState<string>(contracts.tenderly.ibcContract)
+  const [ibcContractAddress, ] = useState<string>(contracts.tenderly.ibcETHCurveContract)
+  const [ibcRouterAddress, ] = useState<string>(contracts.tenderly.ibcRouterContract)
   const {dashboardDataSet} = props
   const [amount, setAmount] = useState<string>('')
 
@@ -61,27 +62,41 @@ export default function StakeIbc(props: mintProps) {
 
       if (userInverseTokenAllowance.gt(0)){
 
-        const functionDescriptorBytes = arrayify(solidityKeccak256(
-          [
-            "string"
-          ]
-          ,
-          [
-            "stake(uint256)" // put function signature here w/ types + no spaces, ex: createPair(address,address)
-          ]
-        )).slice(0,4)
+        const functionDescriptorBytes = arrayify(
+          solidityKeccak256(
+            ['string'],
+            [
+              'execute(address,address,bool,uint8,bytes)', // put function signature here w/ types + no spaces, ex: createPair(address,address)
+            ]
+          )
+        ).slice(0, 4)
           
-        const payloadBytes = arrayify(abiCoder.encode(
+        const commandBytes = arrayify(abiCoder.encode(
           [
+            "address",
             "uint256",
           ], // array of types; make sure to represent complex types as tuples 
           [
+            wallet.accounts[0].address, //ignored via router
             parseUnits(amount.toString(), inverseTokenDecimals)
           ] // arg values
         ))
   
+        const payloadBytes = arrayify(
+          abiCoder.encode(
+            ['address', 'address', 'bool', 'uint8', 'bytes'], // array of types; make sure to represent complex types as tuples
+            [
+              wallet.accounts[0].address,
+              ibcContractAddress,
+              true,
+              commandTypes.stake,
+              commandBytes,
+            ] // arg values
+          )
+        )
+
         txDetails = {
-          to: ibcContractAddress,
+          to: ibcRouterAddress,
           data: hexlify(concat([functionDescriptorBytes, payloadBytes])),
         }
 
@@ -103,7 +118,7 @@ export default function StakeIbc(props: mintProps) {
             "uint",
           ], // array of types; make sure to represent complex types as tuples 
           [
-            ibcContractAddress,
+            ibcRouterAddress,
             constants.MaxUint256
           ] // arg values; note https://docs.ethers.org/v5/api/utils/abi/coder/#AbiCoder--methods
         ))
@@ -149,7 +164,7 @@ export default function StakeIbc(props: mintProps) {
     }
     setIsProcessing(false)
     forceUpdate()
-  }, [wallet, provider, ibcContractAddress, amount, inverseTokenDecimals, userInverseTokenAllowance, inverseTokenAddress]);
+  }, [wallet, provider, ibcContractAddress, amount, inverseTokenDecimals, userInverseTokenAllowance, inverseTokenAddress, ibcRouterAddress]);
 
   return (
     <>

f12dcc99968447f1a2e777c6a26a2e2c6fd082e1
kimchicoffee <pasdesujin@gmail.com>
2023-10-31 12:05:58 -0400

    how it works page content and little style changes (#154)

--- src/components/dashboard/how_it_works.tsx
+++ src/components/dashboard/how_it_works.tsx
@@ -13,70 +13,58 @@ import { BsDash } from 'react-icons/bs'
 
 export default function HowItWorks() {
 	const ibc_holder_stakes = [
-		'Their position value increases as more IBC tokens are burnt following the mint.',
-		'Their position value decreases as more IBC tokens are minted following the mint.',
-		'Mints and burns incur a fee, distributed to IBC stakers and LPs.',
-		'Staked IBC accrues prorated fees from mints and burns and LP adds and removals.',
+		'Mints ibAssets by providing reserve assets (e.g. ETH) to the IBC.',
+		'Their position value increases as more ibAssets are burnt following the mint.',
+		'Their position value decreases as more ibAssets are minted following the mint.',
+		'Mints and burns incur a fee, distributed to ibAsset stakers and LPs.',
+		'Staked ibAssets accrue prorated fees from mints and burns and LP adds and removals.',
 		'Later entrants can acquire a higher stake of fees as their mint price is lower.',
 	]
 
 	const liquidity_providers = [
+		'Provides additional reserve assets (e.g. ETH) to improve the IBC’s market liquidity.',
 		'Their position value increases as the price deviates from the price at the time of LP.',
 		'LPs can maintain only one position per account / address at any given time.',
 		'LPs must close their previous position in full before modifying their position size.',
-		'Adding and removing liquidity incur a fee, distributed to IBC stakers and LPs.',
+		'Adding and removing liquidity incur a fee, distributed to ibAsset stakers and LPs.',
 		'LPs accrue prorated fees from mints and burns and LP adds and removals.',
 		'LPs may be required to provide or receive additional IBC in order for LP removal.',
 	]
 
+	const curve_initializers = [
+		'Creates new ibAssets for promising reserve assets.',
+		'Curve initializers can be the early users of a new ibAsset, including LPing.',
+		'Provides the initial reserves for a new ibAsset and receives a LP position.',
+		'A small fee is taken from the provided initial reserves.',
+	]
+
 	return (
-		<Stack spacing='7' fontSize='md' textAlign='left' p='7'>
+		<Stack spacing='8' fontSize='md' textAlign='left' p='7' fontWeight='normal'>
 			<Text>
-				The inverse bonding curve implementation is the first demonstration of
-				inverse bonding curves; used for minting assets that devalue as bought
-				and rise in value as sold. The token minted via the inverse bonding
-				curve implementation, IBC, is the first asset in history that follows
-				the exact inverse of regular market dynamics.
-			</Text>
-			<Text>
-				Inverse bonding curves are a brand new DeFi primitive derived from
-				bonding curves. The pricing algorithm enforced by the inverse bonding
-				curve shows various unique traits, one of them being its arbitrage
-				characteristics. Arbitrage of IBC forces IBC to devalue per every
+				The inverse bonding curve (IBC) implementation mints assets that devalue
+				as bought and appreciate as sold. Tokens minted via IBCs, ibAssets, are
+				the first asset class in history to follow the exact inverse of regular
+				market dynamics. Arbitrage forces ibAssets to devalue per every
 				purchase, regardless of where the purchase was made (CEX, Uniswap, etc.
-				). Altogether, these distinctive properties of inverse bonding curves
-				allow for the creation of DeFi mechanisms previous unthought of,
-				including but not limited to an oracle-free derivatives protocol [link
-				to be added].
+				).
+			</Text>
+			<Text as='b'>
+				The distinctive properties of IBCs allow for the creation of DeFi
+				mechanisms previously unthought of, one of which is an oracle-free
+				derivatives protocol [hyperlink to be added].
 			</Text>
 			<Text>
-				The purpose of the inverse bonding curve implementation is to increase
-				awareness and understanding of this new primitive, which will help spark
-				DeFi innovations that leverage its features.
+				The purpose of the IBC implementation is to increase awareness and
+				understandings of this new primitive, helping to spark DeFi innovations
+				that leverage its features.
 			</Text>
 			<Box>
-				<Heading as='h5' size='sm'>
-					Basic Protocol Functions
-				</Heading>
-				<Text>
-					Two participant types power the inverse bonding curve implementation:
-					IBC Minters / Stakers and Liquidity Providers (LPs). IBC can be minted
-					by providing reserve assets (ETH) to the inverse bonding curve, from
-					which new IBC tokens are minted into existence and given to the
-					minter. LPs can provide additional ETH to improve the market liquidity
-					of the inverse bonding curve.
-				</Text>
-			</Box>
-
-			<Text>Each participant contains the following characteristics: </Text>
-
-			<Box>
-				<Heading as='h5' size='sm'>
+				<Heading as='h5' size='m'>
 					IBC Holders / Stakers
 				</Heading>
 				<List>
-					{ibc_holder_stakes.map((stake) => (
-						<ListItem>
+					{ibc_holder_stakes.map((stake, i) => (
+						<ListItem key={i}>
 							<ListIcon as={BsDash} />
 							{stake}
 						</ListItem>
@@ -84,12 +72,25 @@ export default function HowItWorks() {
 				</List>
 			</Box>
 			<Box>
-				<Heading as='h5' size='sm'>
+				<Heading as='h5' size='m'>
 					Liquidity Providers (LPs)
 				</Heading>
 				<List>
-					{liquidity_providers.map((stake) => (
-						<ListItem>
+					{liquidity_providers.map((stake, i) => (
+						<ListItem key={i}>
+							<ListIcon as={BsDash} />
+							{stake}
+						</ListItem>
+					))}
+				</List>
+			</Box>
+			<Box>
+				<Heading as='h5' size='m'>
+					Curve Initializers
+				</Heading>
+				<List>
+					{curve_initializers.map((stake, i) => (
+						<ListItem key={i}>
 							<ListIcon as={BsDash} />
 							{stake}
 						</ListItem>

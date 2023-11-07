7fba3533deacf861f526f0b5bb2f9f7b8e850b71
Sammy <changjiu.shao@gmail.com>
2023-11-08 01:22:20 +0800

    push missing file

--- src/config/curves.ts
+++ src/config/curves.ts
@@ -10,7 +10,7 @@ export const curves = [
     {
         ibAsset: 'ibPEPE',
         reserveSymbol: "PEPE",
-        icon: 'ib_pepe_logo.png',
+        icon: 'ib_pepe_logo.svg',
         curveAddress: '0x6E2b3983698eE5F4f8dc9a0809B25962A243fA1F',
         reserveAddress: '0x6982508145454Ce325dDbE47a25d4ec3d2311933',
         ibAssetAddress: '0x7337299eB291496c4aF9fC45483277f2C15dCD41'        

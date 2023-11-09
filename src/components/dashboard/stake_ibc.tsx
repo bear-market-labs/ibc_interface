c37ba10dc37cb0983af542f063c7d463a7ed7730
jc <2389015+ibjc@users.noreply.github.com>
2023-11-08 20:57:12 -0500

    230 bug allowance checks (#231)
    * fix allowance checks
    
    * fix button text logic

--- src/components/dashboard/stake_ibc.tsx
+++ src/components/dashboard/stake_ibc.tsx
@@ -22,7 +22,7 @@ export default function StakeIbc(props: mintProps) {
   const [provider, setProvider] = useState<ethers.providers.Web3Provider | null>()
   const [ibcRouterAddress, ] = useState<string>(contracts.tenderly.ibcRouterContract)
   const {dashboardDataSet} = props
-  const [amount, setAmount] = useState<string>('')
+  const [amount, setAmount] = useState<string>()
 
   const inverseTokenDecimals = BigNumber.from("lpTokenDecimals" in dashboardDataSet ? dashboardDataSet.lpTokenDecimals : '0'); 
   const inverseTokenAddress = "inverseTokenAddress" in dashboardDataSet ? dashboardDataSet.inverseTokenAddress : "";
@@ -59,7 +59,7 @@ export default function StakeIbc(props: mintProps) {
       let description = "Error details"
       let txDetails;
 
-      if (userInverseTokenAllowance.gt(0)){
+      if (userInverseTokenAllowance.gte(parseUnits(amount, inverseTokenDecimals) ?? BigNumber.from(0))){
 
         const functionDescriptorBytes = arrayify(
           solidityKeccak256(
@@ -118,7 +118,7 @@ export default function StakeIbc(props: mintProps) {
           ], // array of types; make sure to represent complex types as tuples 
           [
             ibcRouterAddress,
-            constants.MaxUint256
+            parseUnits(amount, inverseTokenDecimals)
           ] // arg values; note https://docs.ethers.org/v5/api/utils/abi/coder/#AbiCoder--methods
         ))
 
@@ -205,7 +205,7 @@ export default function StakeIbc(props: mintProps) {
           onClick={sendTransaction}
           isDisabled={!isAbleToSendTransaction(wallet, provider, amount) || isProcessing}>
         {
-              userInverseTokenAllowance.gt(0) ? "Stake" : "Approve " + dashboardDataSet.inverseTokenSymbol
+              userInverseTokenAllowance.gte(parseUnits(amount ?? '0', inverseTokenDecimals) ?? BigNumber.from(0)) ? "Stake" : "Approve " + dashboardDataSet.inverseTokenSymbol
         }
         </Button>
       </Stack>

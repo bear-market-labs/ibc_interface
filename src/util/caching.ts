e5d1345f76b42b922676b9de391534f873f74f79
ibjc <2389015+ibjc@users.noreply.github.com>
2023-11-10 22:41:09 +0000

    add caching heleprs

---
+++ src/util/caching.ts
@@ -0,0 +1,22 @@
+const KEY_SEP: string = '__'
+
+export function getStorageKey(address: string, field: string){
+  return address + KEY_SEP + field
+}
+
+export function keyToParts(key: string){
+  return key.split(KEY_SEP).length > 1 ? {
+    address: key.split(KEY_SEP)[0],
+    field: key.split(KEY_SEP)[1]
+  } : {address: '', field: ''}
+}
+
+// fetch from localstorage
+// if not available, generate the multicall query
+export function fetchFromLocal(key: string){
+  return window.localStorage.getItem(key)
+}
+
+export function cacheToLocal(key: string, value: string){
+  window.localStorage.setItem(key, value)
+}

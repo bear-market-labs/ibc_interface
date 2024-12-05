a10da05d1d25b5fe9657133d2e97527268b86e5f
ibjc <2389015+ibjc@users.noreply.github.com>
2024-12-05 11:31:59 -0500

    remove connect widget

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -803,7 +803,6 @@ export function Dashboard( props: dashboardProps ){
                     />
                   </>
                   }
-                  <ConnectWallet />
                 </Stack>
               </Stack>
             </Stack>
